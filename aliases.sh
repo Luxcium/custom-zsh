87ca1b1381c1fc265e6c51c29acadc5f18e1f1f0
Benjamin Vincent <luxcium@neb401.com>
2020-01-28 01:58:50 -0500

    adding functionalities

--- aliases.sh
+++ aliases.sh
@@ -478,15 +478,19 @@ function aliases_functions() {
         push
     }
     function source_load_aliases() {
+        AHMYZSH="${HOME}/ahmyzsh"
+
+        . "${AHMYZSH}/LOAD_ENV.zsh"
+
         . "${AHMYZSH}/PATHS.zsh"
 
         . "${CUSTOM_ZSH}/notice.sh"
 
-        export MY_ALIASES="${CUSTOM_ZSH}/aliases.sh"
+        MY_ALIASES="${CUSTOM_ZSH}/aliases.sh"
         . "${MY_ALIASES}"
         load_aliases
 
-        export BASE_LAYOUTS="${CUSTOM_ZSH}/sources/layouts/base-layouts.sh"
+        BASE_LAYOUTS="${CUSTOM_ZSH}/sources/layouts/base-layouts.sh"
         . "${BASE_LAYOUTS}"
         load_layouts
     }
