d702755cc6d14bc4ce3b313f838f9a260ac93e41
Benjamin Vincent (Luxcium) <luxcium@neb401.com>
2020-10-19 05:30:59 -0400

    merging functiions together
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>

--- MAIN.zsh
+++ MAIN.zsh
@@ -1,3 +1,4 @@
+
 	function activate_normal_prompt() {
 
 		typeset -g ZSH_THEME="../../powerlevel10k/powerlevel10k"
@@ -204,4 +205,100 @@ function load_zlogout() {
 
 	function load_aliases() {
   Load_all_files_d "${AHMYZSH_ENV}/aliases"
+}
+
+function load_options() {
+    bindkey -e
+
+    HISTFILE="${HOME}/.zsh_history"
+    HISTSIZE=1000000
+    SAVEHIST=1000000
+
+    export PROMPT_EOL_MARK=''
+    setopt PROMPT_CR
+    unsetopt PROMPT_SP
+    setopt appendhistory
+    setopt autocd
+    setopt BANG_HIST # Treat the '!' character specially during expansion.
+    setopt beep
+    setopt EXTENDED_HISTORY # Write the history file in the ":start:elapsed;command" format.
+    setopt HIST_BEEP
+    setopt HIST_EXPIRE_DUPS_FIRST # Expire duplicate entries first when trimming history.
+    # setopt HIST_FIND_NO_DUPS      # Do not display a line previously found.
+    # setopt HIST_IGNORE_ALL_DUPS   # Delete old recorded entry if new entry is a duplicate.
+    # setopt HIST_IGNORE_DUPS       # Don't record an entry that was just recorded again.
+    setopt HIST_IGNORE_SPACE  # Don't record an entry starting with a space.
+    setopt HIST_REDUCE_BLANKS # Remove superfluous blanks before recording entry.
+    # setopt HIST_SAVE_NO_DUPS      # Don't write duplicate entries in the history file.
+    setopt HIST_VERIFY        # Don't execute immediately upon history expansion.
+    setopt INC_APPEND_HISTORY # Write to the history file immediately, not when the shell exits.
+    setopt INTERACTIVE_COMMENTS
+    setopt SHARE_HISTORY # Share history between all sessions.
+
+    export TERM="xterm-256color"
+    export CLICOLOR='1'
+    export LSCOLORS='GxFxCxDxBxDgedabagacad'
+    export GPG_TT=(tty)
+
+    # Correctly display UTF-8 with combining characters.
+    if [ "$TERM_PROGRAM" = "Apple_Terminal" ]; then
+        setopt combiningchars
+    fi
+    # disable log
+    [ -r "/etc/zshrc_$TERM_PROGRAM" ] && . "/etc/zshrc_$TERM_PROGRAM"
+
+    TIMEFMT=$'\n================\nCPU\t%P\nuser\t%*U\nsystem\t%*S\ntotal\t%*E'
+
+}
+
+
+function load_oh_my_zsh() {
+    # Uncomment the following line to disable bi-weekly auto-update checks.
+    DISABLE_AUTO_UPDATE="true"
+
+    # Uncomment the following line to disable auto-setting terminal title.
+    DISABLE_AUTO_TITLE="true"
+
+    # Uncomment the following line to enable command auto-correction.
+    # ENABLE_CORRECTION="true"
+
+    # Uncomment the following line to display red dots whilst waiting for completion.
+    COMPLETION_WAITING_DOTS="true"
+
+    # Uncomment the following line if you want to disable marking untracked files
+    # under VCS as dirty. This makes repository status check for large repositories
+    # much, much faster.
+    DISABLE_UNTRACKED_FILES_DIRTY="true"
+    plugins=(
+        # zsh-autosuggestions
+        # zsh-syntax-highlighting
+        git
+        redis-cli
+    )
+    # alias-finder
+    # colorize
+    # dnf
+    # emoji
+    # gem
+    # git
+    # git-auto-fetch
+    # git-hubflow
+    # git-prompt
+    # github
+    # man
+    # node
+    # npm
+    # rbenv
+    # redis-cli
+    # ruby
+    # safe-paste
+    # systemadmin
+    # systemd
+    # tmux
+    # tmuxinator
+    # vscode
+    # yarn
+    source $ZSH/oh-my-zsh.sh
+    unalias ll
+    # echo -n "${normal}$CLRLN$LDLCLR$(tput setaf 2) \uf013 ${bold} DONE! load_oh_my_zsh()${normal}"
 }
