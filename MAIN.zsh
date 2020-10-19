d27a383151c36a530e03bda8c9d84c961eb01604
Benjamin Vincent (Luxcium) <luxcium@neb401.com>
2020-10-19 05:38:07 -0400

    update source files
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>

--- MAIN.zsh
+++ MAIN.zsh
@@ -95,8 +95,8 @@ function load_zshrc() {
     (compute_path &) >/dev/null
   fi
 
-  load_ "${CUSTOM_ZSH}/lib/options-list.zsh" "load_options_list"
-  load_ "${CUSTOM_ZSH}/lib/options.zsh" "load_options"
+  load_ "${CUSTOM_ZSH}/sources/options-list.zsh" "load_options_list"
+  load_options
 
   # https://github.com/zsh-users/zsh-autosuggestions#configuration
   ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#677787"
